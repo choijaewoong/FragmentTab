ca3d2390be02fb6f832b08d16611ec5789d96482
choijaewoong <jwchoi0129@gmail.com>
2015-10-12 11:26:40 +0900

    각 프래그먼트에 메뉴 추가

--- app/src/main/java/com/torerov/fragmenttab/Tab2Fragment.java
+++ app/src/main/java/com/torerov/fragmenttab/Tab2Fragment.java
@@ -4,8 +4,12 @@ package com.torerov.fragmenttab;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 
 /**
@@ -16,6 +20,7 @@ public class Tab2Fragment extends Fragment {
 
     public Tab2Fragment() {
         // Required empty public constructor
+        setHasOptionsMenu(true);
     }
 
 
@@ -24,6 +29,20 @@ public class Tab2Fragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tab2, container, false);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_f2, menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.menu_f2)
+            Toast.makeText(getContext(), "F2M1 selected", Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
     }
 
 
